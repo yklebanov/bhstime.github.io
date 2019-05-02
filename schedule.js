595ad4d24ead2fddd4d7c8012941621d29526756
Jack <harryhogwarts12345@gmail.com>
2019-05-02 12:46:52 -0400

    Made time function

--- schedule.js
+++ schedule.js
@@ -1,2 +1,11 @@
-var d = new Date();
-document.getElementById("test").innerHTML = d.getHours();
+var today = new Date();
+
+var mon = new Map([["A", 30000000]]);
+
+for(let [block, time] of mon){
+  console.log(block + " " + time);
+  if (d < time){
+    document.write(block + " block starts in " + Math.round((time-d)/60) + ".");
+    break;
+  }
+}
